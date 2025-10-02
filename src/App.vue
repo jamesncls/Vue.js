f540279421b3ec3c72decb9e33ec157acfce078d
Nycollas <nycollas@freeline.inf.br>
2025-10-02 17:36:29 -0300

    v-if, v-if-else e v-else

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
+    <TheHeader v-if="showHeader"
+    />
+
+    <div v-show="showName">
+      Nome: {{ firstName }} <br>
+      Sobrenome: {{ lastName }}
+    </div>
+
+    <div v-if="levelAccess === 'Admin'">Admin</div>
+    <div v-else-if="levelAccess === 'Marketing'">Marketing</div>
+    <div v-else>User</div>
+
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TheHeader
+  },
+  data() {
+    return {
+      showHeader: true,
+      firstName: 'Nycollas James',
+      lastName: 'Mavignier',
+      showName: false,
+      levelAccess: 'User',
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    #app {  
+      font-family: Avenir, Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
+    }
+    </style>
