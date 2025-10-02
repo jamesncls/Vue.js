779e99c34cf18778d95d09fef6f3c0a595e803ea
Nycollas <nycollas@freeline.inf.br>
2025-10-02 17:59:34 -0300

    v-for

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,12 +1,65 @@
 <template>
-    <header class="header">
-       TheHeader
-    </header>
+
+    <div>
+
+        <div v-for="obj in todos"
+            
+        v-bind:key="obj.id"
+        
+        >
+            {{ obj.title }}
+        </div>
+
+    </div>
 </template>
 
 <script>
     export default {
-        //name: 'TheHeader',
+        name: 'App',
+        data() {
+            return {
+                todos: [
+                    [
+                        {
+                            "userId": 1,
+                            "id": 1,
+                            "title": "delectus aut autem",
+                            "completed": false
+                        },
+                        {
+                            "userId": 1,
+                            "id": 2,
+                            "title": "quis ut nam facilis et officia qui",
+                            "completed": false
+                        },
+                        {
+                            "userId": 1,
+                            "id": 3,
+                            "title": "fugiat veniam minus",
+                            "completed": false
+                        },
+                        {
+                            "userId": 1,
+                            "id": 4,
+                            "title": "et porro tempora",
+                            "completed": true
+                        },
+                        {
+                            "userId": 1,
+                            "id": 5,
+                            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+                            "completed": false
+                        },
+                        {
+                            "userId": 1,
+                            "id": 6,
+                            "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+                            "completed": false
+                        },
+                        ]
+                ]
+            }
+        }
     }
 
 
